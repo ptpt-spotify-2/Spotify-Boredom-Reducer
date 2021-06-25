c7672557b31d03e9050fbb9797316cd907451ae3
james-caldwell1981 <james.caldwell.1981@gmail.com>
2021-06-25 00:44:13 -0400

    wrapped app and added to init file

--- app/app.py
+++ app/app.py
@@ -2,35 +2,33 @@ from flask import Flask, render_template, jsonify, request
 import API_requests
 
 
-app = Flask(__name__)
-req_handler = API_requests
+def app_run():
+    app = Flask(__name__)
+    req_handler = API_requests
 
 
-@app.route('/')
-def landing():
+    @app.route('/')
+    def landing():
 
-    return render_template('landing.html')
-
-
-@app.route('/get_songs/', methods=['GET'])
-def get_songs():
-
-    q_type = request.args.get('query_type')
-    subject = request.args.get('query_text')
+        return render_template('landing.html')
 
 
-    song_query = req_handler.song_search(q_type, subject)
+    @app.route('/get_songs/', methods=['GET'])
+    def get_songs():
 
-    model_input_df = req_handler.get_model_input({
-      'id': song_query['track_id'], 'popularity': song_query['popularity']})
+        q_type = request.args.get('query_type')
+        subject = request.args.get('query_text')
 
-    suggested_song_indices = req_handler.model_prediction(model_input_df)
 
-    predicted_track_id_list = req_handler.get_tracks_model(suggested_song_indices)['tracks']
+        song_query = req_handler.song_search(q_type, subject)
 
-    track_items = req_handler.get_query_id(predicted_track_id_list, 'track')
-    print(track_items)
-    return 'Holy Crapola Spotify!'
+        model_input_df = req_handler.get_model_input({
+          'id': song_query['track_id'], 'popularity': song_query['popularity']})
 
-if __name__ == '__main__':
-    app.run()
+        suggested_song_indices = req_handler.model_prediction(model_input_df)
+
+        predicted_track_id_list = req_handler.get_tracks_model(suggested_song_indices)['tracks']
+
+        track_items = req_handler.get_query_id(predicted_track_id_list, 'track')
+        print(track_items)
+        return 'Holy Crapola Spotify!'
